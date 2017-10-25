d59671dc538367dce84323b7781f352d049a6912
LQ <liqiangmob@163.com>
2017-10-25 16:52:43 +0800

    封装搞定

--- app/src/main/java/com/example/lq/viewcommonitem/ViewCommonItem.java
+++ app/src/main/java/com/example/lq/viewcommonitem/ViewCommonItem.java
@@ -2,9 +2,8 @@ package com.example.lq.viewcommonitem;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.text.TextUtils;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -24,11 +23,10 @@ public class ViewCommonItem extends RelativeLayout {
     TextView itemDescription;
     ImageView itemRightImage;
     View itemBottomLine;
-//    RelativeLayout container;
 
 
-    private int defaultImageWidth = 18;
-    private int defaultImageHeight = 18;
+    private int defaultImageWidth = 0;
+    private int defaultImageHeight = 0;
     private int defaultImageLeftMargin = 20;
 
     private int ImageWidth;
@@ -36,28 +34,26 @@ public class ViewCommonItem extends RelativeLayout {
     private int ImageLeftMargin;
 
 
-    private boolean defaultTvTitleSingle = false;
-    private float defaultTvTitleSize = 14;
+    private int defaultTvTitleSize = 14;
     private int defaultTvTitleColor;
     private int defaultTvTitleLeftMargin = 5;
 
-    private boolean tvTitleSingle = false;
     private float tvTitleSize;
     private int tvTitleColor;
     private int tvTitleLeftMargin;
 
-    private boolean defaultTvDescriptionSingle = true;
-    private float defaultTvDescriptionSize = 12;
+
+    private int defaultTvDescriptionSize = 12;
     private int defaultTvDescriptionColor;
     private int defaultTvDescriptionRightMargin = 20;
 
-    private boolean tvDescriptionSingle;
+
     private float tvDescriptionSize;
     private int tvDescriptionColor;
     private int tvDescriptionRightMargin;
 
-    private int defaultRightImgHeight = 18;
-    private int defaultRightImgWidth = 8;
+    private int defaultRightImgHeight = 0;
+    private int defaultRightImgWidth = 0;
     private int defaultRightImgRightMargin = 20;
 
     private int rightImgHeight;
@@ -79,7 +75,6 @@ public class ViewCommonItem extends RelativeLayout {
         itemDescription = new TextView(mContext);
         itemRightImage = new ImageView(mContext);
         itemBottomLine = new View(mContext);
-//        container = new RelativeLayout(mContext);
 
     }
 
@@ -97,6 +92,7 @@ public class ViewCommonItem extends RelativeLayout {
         TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.ViewCommonItem);
         if (attributes != null) {
 
+
             initImageView(attributes);
 
             initItemTitle(attributes);
@@ -113,63 +109,73 @@ public class ViewCommonItem extends RelativeLayout {
 
     private void initImageView(TypedArray attributes) {
 
-
+        itemImage.setId(R.id.item_img);
+        itemImage.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
         ImageWidth = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_img_width, defaultImageWidth);
         ImageHeight = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_img_height, defaultImageHeight);
         ImageLeftMargin = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_img_left_margin, defaultImageLeftMargin);
 
         Glide.with(mContext).load(attributes.getResourceId(R.styleable.ViewCommonItem_img_src, R.mipmap.ic_launcher)).into(itemImage);
+        if (ImageHeight == 0 || ImageWidth == 0) {
+            ImageHeight = LayoutParams.WRAP_CONTENT;
+            ImageWidth = LayoutParams.WRAP_CONTENT;
+        }
 
         LayoutParams layoutParams = new LayoutParams(ImageWidth, ImageHeight);
-        layoutParams.addRule(Gravity.CENTER_VERTICAL, TRUE);
+
         layoutParams.setMargins(ImageLeftMargin, 0, 0, 0);
+        layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
+        layoutParams.addRule(RelativeLayout.ALIGN_LEFT);
         addView(itemImage, layoutParams);
 
     }
 
     private void initItemTitle(TypedArray attributes) {
-
-
-        tvTitleSize = attributes.getDimension(R.styleable.ViewCommonItem_tv_title_size, defaultTvTitleSize);
+        itemTitle.setId(R.id.item_title);
+        tvTitleSize = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_tv_title_size, defaultTvTitleSize);
         tvTitleColor = attributes.getColor(R.styleable.ViewCommonItem_tv_title_color, 0);
-        tvTitleLeftMargin = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_img_left_margin, defaultTvTitleLeftMargin);
-        tvTitleSingle = attributes.getBoolean(R.styleable.ViewCommonItem_tv_title_single, defaultTvTitleSingle);
+        tvTitleLeftMargin = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_tv_title_left_margin, defaultTvTitleLeftMargin);
 
         String title = attributes.getString(R.styleable.ViewCommonItem_tv_title);
 
-        itemTitle.setText(StringUtil.safe(title));
-        itemTitle.setTextColor(tvTitleColor);
-        itemTitle.setTextSize(tvTitleSize);
-        if (tvTitleSingle) {
-            itemTitle.setSingleLine();
-            itemDescription.setEllipsize(TextUtils.TruncateAt.END);
+        itemTitle.setText(Utils.safe(title));
+        if (tvTitleColor == 0) {
+            itemTitle.setTextColor(ContextCompat.getColor(mContext, R.color.colorPrimary));
+        } else {
+            itemTitle.setTextColor(tvTitleColor);
         }
-        LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.addRule(Gravity.CENTER_VERTICAL, TRUE);
-        layoutParams.setMargins(ImageLeftMargin, 0, 0, 0);
-        addView(itemImage, layoutParams);
+
+        itemTitle.setTextSize(Utils.px2sp(mContext, tvTitleSize));
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
+        layoutParams.addRule(RelativeLayout.RIGHT_OF, R.id.item_img);
+        layoutParams.setMargins(tvTitleLeftMargin, 0, 0, 0);
+
+        addView(itemTitle, layoutParams);
 
 
     }
 
     private void initItemDescription(TypedArray attributes) {
 
-        tvDescriptionSize = attributes.getDimension(R.styleable.ViewCommonItem_tv_description_size, defaultTvDescriptionSize);
+        itemDescription.setId(R.id.item_des);
+        tvDescriptionSize = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_tv_description_size, defaultTvDescriptionSize);
         tvDescriptionColor = attributes.getColor(R.styleable.ViewCommonItem_tv_description_color, 0);
         tvDescriptionRightMargin = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_tv_description_right_margin, defaultTvDescriptionRightMargin);
-        tvDescriptionSingle = attributes.getBoolean(R.styleable.ViewCommonItem_tv_description_single, defaultTvDescriptionSingle);
 
         String description = attributes.getString(R.styleable.ViewCommonItem_tv_description);
-        itemDescription.setText(StringUtil.safe(description));
-        itemDescription.setTextColor(tvDescriptionColor);
-        itemDescription.setTextSize(tvDescriptionSize);
-        if (tvDescriptionSingle) {
-            itemDescription.setSingleLine();
-            itemDescription.setEllipsize(TextUtils.TruncateAt.END);
+        itemDescription.setText(Utils.safe(description));
+        itemDescription.setTextSize(Utils.px2sp(mContext, tvDescriptionSize));
+        if (tvDescriptionColor == 0) {
+            itemDescription.setTextColor(ContextCompat.getColor(mContext, R.color.colorPrimary));
+        } else {
+            itemDescription.setTextColor(tvDescriptionColor);
         }
 
         LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.addRule(Gravity.CENTER_VERTICAL, TRUE);
+        layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
+        layoutParams.addRule(RelativeLayout.LEFT_OF, R.id.item_right_img);
         layoutParams.setMargins(0, 0, tvDescriptionRightMargin, 0);
         addView(itemDescription, layoutParams);
 
@@ -178,15 +184,23 @@ public class ViewCommonItem extends RelativeLayout {
 
     private void initRightImageView(TypedArray attributes) {
 
-
-        rightImgWidth = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_img_width, defaultRightImgWidth);
-        rightImgHeight = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_img_height, defaultRightImgHeight);
-        rightImgRightMargin = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_img_left_margin, defaultRightImgRightMargin);
+        itemRightImage.setId(R.id.item_right_img);
+        itemRightImage.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        rightImgWidth = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_right_img_width, defaultRightImgWidth);
+        rightImgHeight = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_right_img_height, defaultRightImgHeight);
+        rightImgRightMargin = attributes.getDimensionPixelSize(R.styleable.ViewCommonItem_right_img_right_margin, defaultRightImgRightMargin);
 
         Glide.with(mContext).load(attributes.getResourceId(R.styleable.ViewCommonItem_right_img, R.mipmap.ic_launcher)).into(itemRightImage);
 
-        LayoutParams layoutParams = new LayoutParams(rightImgWidth, rightImgHeight);
-        layoutParams.addRule(Gravity.CENTER_VERTICAL, TRUE);
+        if (rightImgWidth == 0 || rightImgHeight == 0) {
+            rightImgWidth = LayoutParams.WRAP_CONTENT;
+            rightImgHeight = LayoutParams.WRAP_CONTENT;
+        }
+
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(rightImgWidth, rightImgHeight);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
         layoutParams.setMargins(0, 0, rightImgRightMargin, 0);
         addView(itemRightImage, layoutParams);
 
@@ -194,27 +208,52 @@ public class ViewCommonItem extends RelativeLayout {
 
 
     private void initItemBottomLine(TypedArray attributes) {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        itemBottomLine.setBackgroundColor(ContextCompat.getColor(mContext, R.color.orange_00000));
+
         if (attributes.getBoolean(R.styleable.ViewCommonItem_bottom_line_show, defaultBottomLineShow)) {
-            itemBottomLine.setVisibility(VISIBLE);
-        } else {
-            itemBottomLine.setVisibility(GONE);
+            addView(itemBottomLine, layoutParams);
+        }
+
+    }
+
+
+    private void setData(String title, String des) {
+        if (itemTitle != null) {
+            itemTitle.setText(Utils.safe(title));
+        }
+        if (itemDescription != null) {
+            itemDescription.setText(Utils.safe(des));
+        }
+
+    }
+
+    private void setData(String title, String imgUrl, String des) {
+        if (itemTitle != null) {
+            itemTitle.setText(Utils.safe(title));
+        }
+        if (itemDescription != null) {
+            itemDescription.setText(Utils.safe(des));
+        }
+        if (itemImage != null) {
+
+            Glide.with(mContext).load(imgUrl).into(itemImage);
         }
     }
 
+    private void setData(String title, int imgUrl, String des) {
+        if (itemTitle != null) {
+            itemTitle.setText(Utils.safe(title));
+        }
+        if (itemDescription != null) {
+            itemDescription.setText(Utils.safe(des));
+        }
+        if (itemImage != null) {
+            Glide.with(mContext).load(imgUrl).into(itemImage);
 
-    public void setData(int imageResource, String sTitle, String sDescription) {
-
-        Glide.with(mContext).load(imageResource).into(itemImage);
-        itemTitle.setText(StringUtil.safe(sTitle));
-        itemDescription.setText(StringUtil.safe(sDescription));
-
-
+        }
     }
 
-
-    public int dip2px(Context context, float dipValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dipValue * scale + 0.5f);
-    }
 
 }
